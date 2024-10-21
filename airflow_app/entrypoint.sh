548bc2ab66dca3db53941ef4eeef582ed2b1b7db
Sean Deery <sdeery14@gmail.com>
2024-10-20 21:16:16 -0400

    add airflow init service

--- airflow_app/entrypoint.sh
+++ airflow_app/entrypoint.sh
@@ -12,5 +12,17 @@ else
   echo "Database already initialized."
 fi
 
+# Check if the Airflow user exists, and create it if it doesn't
+airflow users list | grep -q "${AIRFLOW_ADMIN_USERNAME}" || {
+  echo "Creating Airflow admin user..."
+  airflow users create \
+    --username "${AIRFLOW_ADMIN_USERNAME}" \
+    --firstname "${AIRFLOW_ADMIN_FIRSTNAME}" \
+    --lastname "${AIRFLOW_ADMIN_LASTNAME}" \
+    --role "${AIRFLOW_ADMIN_ROLE}" \
+    --email "${AIRFLOW_ADMIN_EMAIL}" \
+    --password "${AIRFLOW_ADMIN_PASSWORD}"
+}
+
 # Execute the main command passed to the script
 exec "$@"
